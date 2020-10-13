961810a5dec8183eb46aa0299aa743d75769b51e
AntoninaY01 <a.yordanova01@icloud.com>
2020-10-13 16:16:11 +0000

    2

--- src/exercise3/PrimeNumber.java
+++ src/exercise3/PrimeNumber.java
@@ -7,10 +7,15 @@ public class PrimeNumber{
   public static void main(String[] args){
  Scanner input = new Scanner (System.in);
  int number = input.nextInt();
-
- while(number>2){
-
- }
- 
+ int d = 2;
+ boolean prime = true;
+ while(d<number){
+   if(number%d==0){
+   prime = false;
+     break;
+      }
+     d++;
+     }
+     System.out.println("Prime number: " + prime);
   }
 }
